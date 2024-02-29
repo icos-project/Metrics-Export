da79880b490d9be4692b62b63ff3a6ab9d1218ce
Zetas Menelaos - x_menzetas <menelaoszetas@gmail.com>
2024-02-29 18:46:18 +0200

    added the helm chart for a PoC of a gauge message sent

--- metrics_generator.py
+++ metrics_generator.py
@@ -1,10 +1,13 @@
 import os
-import time
 from flask import Flask, request, jsonify
 from prometheus_client import Gauge, make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
+import logging
+from datetime import datetime
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 # Initialize Flask
 app = Flask(__name__)
 
@@ -18,10 +21,13 @@ def process_model():
     data = request.json
     model = data.get('model')
     result = data.get('result')
+
+    logger.info('Time: {}, model: {}, result: {}'.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3], model,
+                                                         result))
+
     if not model or result is None:
         return jsonify({'error': 'Model and result are required.'}), 400
 
-    g.labels(model=model).set_to_current_time()   # Set to current unixtime
     # Set the gauge with the model label and result value
     g.labels(model=model).set(result)
     return jsonify({'message': 'Metric updated successfully.'}), 200
@@ -36,27 +42,3 @@ if __name__ == '__main__':
     # Start up the server to expose the metrics and Flask app
     port = int(os.getenv('PORT', '8000'))
     run_simple(hostname="0.0.0.0", port=port, application=app_dispatch)
-
-
-# FOR TESTING PURPOSE
-# metrics_generator.py
-# import time
-# from prometheus_client import start_http_server, Gauge
-#
-# g = Gauge('intelligence_layer_model', 'intelligence layer info', ['model'])
-#
-#
-# def process_model(model, result):
-#     g.labels(model=model).set(result)
-#
-#
-# if __name__ == '__main__':
-#     # Start up the server to expose the metrics.
-#     start_http_server(8000)
-#     # Generate some requests.
-#     counter = 1
-#     while True:
-#         counter = counter + 1
-#         # process_model('model_name_{}'.format(counter), str(counter))
-#         process_model('model_name_1'.format(counter), str(counter))
-#         time.sleep(1)
