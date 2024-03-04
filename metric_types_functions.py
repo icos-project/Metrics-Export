606d2cfb01b3777710e4afeb0cf2287dee8ee8c8
Zetas Menelaos - x_menzetas <menelaoszetas@gmail.com>
2024-03-04 21:44:52 +0200

    initial

---
+++ metric_types_functions.py
@@ -0,0 +1,84 @@
+from prometheus_client import Gauge, Counter, Info
+
+
+def counter(metric_name, metric_info, labels, value):
+    """
+    Counters go up, and reset when the process restarts.
+
+    :param metric_name: The metric name
+    :param metric_info: The metric info
+    :param labels: The labels that will pass for the metric
+    :param value: The value of the counter
+    :return: null
+    """
+    if metric_info is None:
+        metric_info = metric_name + ' info'
+    if labels:
+        # Extract keys and store them in a list
+        labels_keys = list(labels.keys())
+    else:
+        labels_keys = []
+
+    # Initialize a counter metric
+    if labels_keys:
+        c = Counter(metric_name, metric_info, labels_keys)
+        # Set the gauge with labels from labels_dict and the value provided.
+        c.labels(**labels).inc(value)
+    else:
+        c = Counter(metric_name, metric_info, [])
+        c.inc(value)
+    return c
+
+
+def gauge(existing_metric, metric_name, metric_info, labels, value):
+    """
+    Gauges can go up and down.
+
+    :param existing_metric: The already existing metric if found
+    :param metric_name: The metric name
+    :param metric_info: The metric info
+    :param labels: The labels that will pass for the metric
+    :param value: The value of the gauge
+    :return: null
+    """
+    if metric_info is None:
+        metric_info = metric_name + ' info'
+    if labels:
+        # Extract keys and store them in a list
+        labels_keys = list(labels.keys())
+    else:
+        labels_keys = []
+
+    if existing_metric is None:
+        # Initialize a gauge metric
+        if labels_keys:
+            g = Gauge(metric_name, metric_info, labels_keys)
+            # Set the gauge with labels from labels_dict and the value provided.
+            g.labels(**labels).set(value)
+        else:
+            g = Gauge(metric_name, metric_info, [])
+            g.set(value)
+    else:
+        if labels_keys:
+            # Set the gauge with labels from labels_dict and the value provided.
+            existing_metric.labels(**labels).set(value)
+        else:
+            existing_metric.set(value)
+
+
+def info(metric_name, metric_info, value):
+    """
+    Info tracks key-value information, usually about a whole target.
+
+    :param metric_name: The metric name
+    :param metric_info: The metric info
+    :param value: The value of the info
+    :return: null
+    """
+    if metric_info is None:
+        metric_info = metric_name + ' info'
+
+    # Initialize an info metric
+    i = Info(metric_name, metric_info)
+    i.info(value)
+    return i
---
+++ metrics_generator_v2.py
@@ -0,0 +1,114 @@
+import os
+from flask import Flask, request, jsonify
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+import logging
+from datetime import datetime
+from enum import Enum
+from metric_types_functions import counter, gauge, info
+from prometheus_client import make_wsgi_app, REGISTRY
+from prometheus_client.core import CollectorRegistry
+from prometheus_client.multiprocess import MultiProcessCollector
+
+# get the registry of the metrics
+registry = CollectorRegistry()
+MultiProcessCollector(registry)
+
+# set a typical logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+# Initialize Flask
+app = Flask(__name__)
+
+
+# create the enums of metric types
+class MetricType(Enum):
+    Counter = 1
+    Gauge = 2
+    # Summary = 3
+    # Histogram = 4
+    Info = 5
+    # Enum = 6
+    # Exemplars = 7
+
+
+# Function to get an existing metric by name from the registry
+def get_metric_by_name(metric_name):
+    for collector in REGISTRY._collector_to_names.keys():
+        # Check if this collector is the one we're looking for based on its name
+        if metric_name in REGISTRY._collector_to_names[collector]:
+            return collector
+    return None
+
+
+
+@app.route('/process_model', methods=['POST'])
+def process_model():
+    """
+    process_model route will receive a json payload with a model and a result property.
+    Model property is the name of the model.
+    Result property is the value of the model prediction.
+    After getting the properties it creates a gauge metric that has as a property the model name and as a value the
+    result value.
+
+    :return: a json response with 400 if error occurs or 200 if metric is saved successfully.
+    """
+    # Parse model and result from the request
+    data = request.json
+    # get the metrics type value. Default is Gauge
+    metric_type_value = data.get('type_of_metric', 2)
+
+    # Convert the incoming metric type value to MetricType enum
+    try:
+        metric_type = MetricType(metric_type_value)
+    except ValueError:
+        return {'error': 'Invalid metric type'}, 400
+
+    # get the metric name
+    metric_name = data.get('metric_name', None)
+    # get the value passed
+    value = data.get('value', None)
+
+    # if metric name or value are not passed then return error
+    if metric_name is None or value is None:
+        return jsonify({'error': 'metric_name and value are required.'}), 400
+
+    # check if metric already exists
+    # if it already exists then just update it at the metric runs
+    existing_metric = get_metric_by_name(metric_name)
+
+    # get metric info
+    metric_info = data.get('metric_info', None)
+    # get metric labels
+    labels = data.get('labels', {})
+
+    logger.info('Time: {}, metrics name: {}, value: {}'.format(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3],
+                                                               metric_name, value))
+
+    # Update the appropriate metric based on the enum
+    if metric_type == MetricType.Counter:
+        metric = counter(existing_metric, metric_name, metric_info, labels, value)
+    elif metric_type == MetricType.Gauge:
+        metric = gauge(existing_metric, metric_name, metric_info, labels, value)
+    # elif metric_type == MetricType.Summary:
+    # elif metric_type == MetricType.Histogram:
+    elif metric_type == MetricType.Info:
+        metric = info(existing_metric, metric_name, metric_info, value)
+    # elif metric_type == MetricType.Enum:
+    # elif metric_type == MetricType.Exemplars:
+
+    # use it for future to keep track of metric name and labels
+    print(metric)
+
+    return jsonify({'message': 'Metric updated successfully.'}), 200
+
+
+# Combine Flask app and Prometheus metrics app
+app_dispatch = DispatcherMiddleware(app, {
+    '/metrics': make_wsgi_app(registry)
+})
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics and Flask app
+    port = int(os.getenv('PORT', '8000'))
+    run_simple(hostname="0.0.0.0", port=port, application=app_dispatch)
